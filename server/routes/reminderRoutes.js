2de6a57516a5c9fe17c22e477c25f16aae4340eb
Ayush Banka <135512030+Ayush-Banka@users.noreply.github.com>
2025-02-08 23:34:24 +0530

    Update reminderRoutes.js

--- server/routes/reminderRoutes.js
+++ server/routes/reminderRoutes.js
@@ -3,16 +3,9 @@ const router = express.Router();
 const reminderController = require("../controllers/reminderController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-// Create Reminder
 router.post("/", authMiddleware, reminderController.createReminder);
-
-// Get All Reminders
 router.get("/", authMiddleware, reminderController.getAllReminders);
-
-// Update Reminder (optional)
 router.put("/:id", authMiddleware, reminderController.updateReminder);
-
-// Delete Reminder
 router.delete("/:id", authMiddleware, reminderController.deleteReminder);
 
 module.exports = router;
